c00f2f456912ca925485b2de14fafbcf02e8a300
JairoRobertoG <jairo_roberto_8@hotmail.com>
2016-05-15 21:40:03 -0600

    Cambio de color a la Fecha

--- mas/Form1.Designer.cs
+++ mas/Form1.Designer.cs
@@ -125,6 +125,7 @@
             // 
             this.lblFechaActual.AutoSize = true;
             this.lblFechaActual.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFechaActual.ForeColor = System.Drawing.SystemColors.ActiveCaption;
             this.lblFechaActual.Location = new System.Drawing.Point(713, 15);
             this.lblFechaActual.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblFechaActual.Name = "lblFechaActual";
@@ -654,7 +655,7 @@
             this.tabPage2.Location = new System.Drawing.Point(4, 25);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(952, 489);
+            this.tabPage2.Size = new System.Drawing.Size(952, 335);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "tabPage2";
             this.tabPage2.UseVisualStyleBackColor = true;
